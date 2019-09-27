45e33c77c714d2555009ead288d6796dca729bc4
Nicolás Bugliot <nicobugliot@gmail.com>
2019-09-27 11:26:13 -0300

    arreglo de currency exception

--- src/main/java/com/example/project/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/project/exception/CustomExceptionHandler.java
@@ -18,8 +18,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(InvalidCurrencyException.class)
     public final ResponseEntity handleUserNotFoundException(InvalidCurrencyException ex, WebRequest request) {
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
+        String details = ex.getLocalizedMessage();
         ErrorResponse error = new ErrorResponse("Record Not Found", details);
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
